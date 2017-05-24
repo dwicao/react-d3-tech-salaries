aa2dfe565152e825a56e7da8fdfa50cdc9c4c80d
dwicao <lutfiandwica@gmail.com>
2017-05-24 08:08:29 +0700

    load the datasets

--- src/DataHandling.js
+++ src/DataHandling.js
@@ -36,3 +36,22 @@ const cleanUSStateName = (d) => ({
   id: Number(d.id),
   name: d.name,
 });
+
+export const loadAllData = (callback = _.noop) => {
+  d3.queue()
+    .defer(d3.json, 'data/us.json')
+    .defer(d3.csv, 'data/us-county-names-normalized.csv')
+    .defer(d3.csv, 'data/county-median-incomes.csv', cleanIncomes)
+    .defer(d3.csv, 'data/h1bs-2012-2016-shortened.csv', cleanSalary)
+    .defer(d3.tsv, 'data/us-state-names.tsv', cleanUSStateName)
+    .await((
+      error,
+      us,
+      countyNames,
+      medianIncomes,
+      techSalaries,
+      USstateNames,
+    ) => {
+
+    });
+};
